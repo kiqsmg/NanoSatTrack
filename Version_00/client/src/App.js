1975e2474336135496b3ea33f4756db674d14656
kiqsmg <kiqsmg@gmail.com>
2024-01-08 16:02:40 -0300

    Set up Colors and MUI light and dark mode themes with redux toolkit

--- Version_00/client/src/App.js
+++ Version_00/client/src/App.js
@@ -1,8 +1,20 @@
 
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+
+
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 }
--- Version_00/client/src/index.js
+++ Version_00/client/src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { configureStore } from "@reduxjs/tollkit";
+import globalReducer from "state";
+import { Provider } from "react-redux";
+
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
