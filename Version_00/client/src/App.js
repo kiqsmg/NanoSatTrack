7f2aef2b608e793e6e218f20714ce0b3949e03c1
kiqsmg <kiqsmg@gmail.com>
2024-01-15 13:36:03 -0300

    Build NavBar with MUI

--- Version_00/client/src/App.js
+++ Version_00/client/src/App.js
@@ -3,7 +3,11 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard";
+
 
 
 function App() {
@@ -12,9 +16,17 @@ function App() {
 
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ Version_00/client/src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+import { LightModeOutlined, DarkModeOutlined, Menu as MenuIcon, Search, SettingsOutlined, ArrowDropDownOutlined, } from "@mui/icons-material";
+import FlexBetween from "components/FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
+import profileImage from "assets/profile.jpg"
+import {
+    AppBar,
+    Button,
+    Box,
+    Typography,
+    IconButton,
+    InputBase,
+    Toolbar,
+    Menu,
+    MenuItem,
+    useTheme,
+  } from "@mui/material";
+
+const Navbar = () => {
+    const dispatch = useDispatch();
+    const theme = useTheme();
+
+
+    return (
+        <AppBar
+            sx={{
+                position: "static",
+                background: "none",
+                boxShadow:"none",
+            }}
+        >
+            <Toolbar sx={{ justifyContent: "space-between"}}>
+                {/* LEFT SIDE */}
+                <FlexBetween>
+                    <IconButton onclick={() => console.log('open/close sidebar')}>
+                        <MenuIcon />
+                    </IconButton>
+                    <FlexBetween
+                        backgroundColor={theme.palette.background.alt}
+                        borderRadius= "9px"
+                        gap= "3rem"
+                        padding= "0.1rem 1.5rem"  /* (Top and Botton) and (left and right) */
+                    >
+                        <InputBase placeholder="Search..." />
+                        <IconButton>
+                            <Search />
+                        </IconButton>
+                    </FlexBetween>
+                </FlexBetween>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar
