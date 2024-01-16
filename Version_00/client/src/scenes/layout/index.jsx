4c4d52588e030771476e587df549e461be1c72c2
kiqsmg <kiqsmg@gmail.com>
2024-01-16 11:00:49 -0300

    Adding new Sidebar with MUI

--- Version_00/client/src/scenes/layout/index.jsx
+++ Version_00/client/src/scenes/layout/index.jsx
@@ -3,14 +3,27 @@ import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
+
 
 
 
 const Layout = () => {
+    const isNonMobile = useMediaQuery("(min-width: 600px)");  //identify if the screen is mobile or not
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
     return (
-        <Box width= "100%" height= "100%">
+        <Box display={isNonMobile ? "flex" : "block" } /* flex for desktop and block for mobile */  width= "100%" height= "100%">
+            <Sidebar 
+            isNonMobile={isNonMobile}
+            drawerWidth="250px"
+            isSidebarOpen={isSidebarOpen}
+            setIsSidebarOpen={setIsSidebarOpen}
+            />
             <Box>
-                <Navbar />
+                <Navbar 
+                    isSidebarOpen={isSidebarOpen}
+                    setIsSidebarOpen={setIsSidebarOpen}
+                />
                 <Outlet />
             </Box>
         </Box>
