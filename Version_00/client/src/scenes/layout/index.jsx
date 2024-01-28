26c01dcdb5ae6b713cc64add1de1a823fa6ee244
kiqsmg <kiqsmg@gmail.com>
2024-01-28 18:29:35 -0300

    Fix the flex position  of the navbar

--- Version_00/client/src/scenes/layout/index.jsx
+++ Version_00/client/src/scenes/layout/index.jsx
@@ -24,7 +24,7 @@ const Layout = () => {
             isSidebarOpen={isSidebarOpen}
             setIsSidebarOpen={setIsSidebarOpen}
             />
-            <Box>
+            <Box flexGrow={1}>
                 <Navbar 
                     isSidebarOpen={isSidebarOpen}
                     setIsSidebarOpen={setIsSidebarOpen}
