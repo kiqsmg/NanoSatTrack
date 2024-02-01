6137d7c66e5b808838bd0dc6afe996133631d072
kiqsmg <kiqsmg@gmail.com>
2024-02-01 16:58:09 -0300

    Add products and prductStats to our Mongo data base

--- Version_01/server/index.js
+++ Version_01/server/index.js
@@ -13,7 +13,9 @@ import salesRoutes from "./routes/sales.js";
 
 // data imports
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
 
 
 
@@ -49,6 +51,8 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     // ONLY ADD DATA ONE TIME
+    Product.insertMany(dataProduct);
+    ProductStat.insertMany(dataProductStat);
     //User.insertMany(dataUser);
 
 }).catch((error) => console.log(`${error} did not connect`));
