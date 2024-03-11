3f31d7b8d7d664e3ebdef5809317929e9436df33
kiqsmg <kiqsmg@gmail.com>
2024-03-11 19:47:39 -0300

    Finalize the code to display the data in the client card

--- Version_01/server/controllers/client.js
+++ Version_01/server/controllers/client.js
@@ -6,18 +6,19 @@ export const getProducts = async (req, res) => {
         const products = await Product.find();
 
         const productsWithStats = await Promise.all(
-            product.map(async (product) => {
+            products.map(async (product) => {
                 const stat = await ProductStat.find({
                     productId: product._id
                 })
                 return {
                     ...product._doc,
                     stat,
-                }
+                };
             })
-        )
-
+        );
+        
+        res.status(200).json(productsWithStats);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
-}
+};
