b0df5068f214b3766ef94dfa21ea861a00582330
kiqsmg <kiqsmg@gmail.com>
2024-03-19 12:36:48 -0300

    Upload transaction data to the mongodb datacloud and create Transaction server models

--- Version_01/server/index.js
+++ Version_01/server/index.js
@@ -14,7 +14,7 @@ import salesRoutes from "./routes/sales.js";
 import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
-//import Transaction from "./models/Transaction.js";
+import Transaction from "./models/Transaction.js";
 //import OverallStat from "./models/OverallStat.js";
 //import AffiliateStat from "./models/AffiliateStat.js";
 import {
@@ -22,8 +22,6 @@ import {
   dataProduct,
   dataProductStat,
   dataTransaction,
-  dataOverallStat,
-  dataAffiliateStat,
 } from "./data/index.js";
 
 
@@ -61,6 +59,7 @@ mongoose
     // ONLY ADD DATA ONE TIME
     //Product.insertMany(dataProduct);
     //ProductStat.insertMany(dataProductStat);
+    //Transaction.insertMany(dataTransaction);
     //User.insertMany(dataUser);
 
 }).catch((error) => console.log(`${error} did not connect`));
