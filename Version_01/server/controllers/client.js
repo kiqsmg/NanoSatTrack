206374d99b609614adfddd42bc944e6400a32d71
kiqsmg <kiqsmg@gmail.com>
2024-03-21 11:31:02 -0300

    Create API endpoint that fetches transactions, allowing pagination, sorting and searching based on certain criteria

--- Version_01/server/controllers/client.js
+++ Version_01/server/controllers/client.js
@@ -36,23 +36,23 @@ export const getCustomers = async (req, res) => {
 
 
 //transaction query
-export const getTransactions = async (req, res) => {
+export const getTransactions = async (req, res) => {  //route handler(fetch transactions)
     try {
         //sort should look like this: { "field": "userId", "sort": "desc"}
-        const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
+        const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;  //Parsing Request Parameters(extract parameters from the requesr query string)
 
         //formated sort should look like this {userId: -1}
-        const generateSort = () => {
+        const generateSort = () => { //formats the sort to MongoDB sor format
             const sortParsed = JSON.parse(sort);
             const sortFormatted = {
-                [sortParsed.field]: sortParsed.sort = "asc" ? 1 : -1
+                [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1 )
             };
 
             return sortFormatted;
         }
         const sortFormatted = Boolean(sort) ? generateSort() : {};
 
-        const transactions = await Transaction.find({
+        const transactions = await Transaction.find({ //consults the data base using the search criteria and pagination paramters
             $or: [
                 { cost: {$regex: new RegExp(search, "i") } },
                 { userId: {$regex: new RegExp(search, "i") } },          
@@ -63,16 +63,16 @@ export const getTransactions = async (req, res) => {
             .limit(pageSize);
 
 
-        const total = await Transaction.countDocuments ({
+        const total = await Transaction.countDocuments ({ //counts the number of documents in the collections based on the search
             name: { $regex: search, $options: "i" }
         });
 
 
-        res.status(200).json({
+        res.status(200).json({ //sends response
             transactions,
             total,
         });
-    } catch (error) {
+    } catch (error) {  //error handling
         res.status(404).json({ message: error.message});
     }
-}
+};
