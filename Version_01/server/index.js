eb69c6161db74dcc80aa3c72e0540667c1c79b86
kiqsmg <kiqsmg@gmail.com>
2024-03-24 11:53:10 -0300

    Import OverallStat data and insert OverllStats documents into MongoDB collection

--- Version_01/server/index.js
+++ Version_01/server/index.js
@@ -15,13 +15,14 @@ import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
-//import OverallStat from "./models/OverallStat.js";
+import OverallStat from "./models/OverallStat.js";
 //import AffiliateStat from "./models/AffiliateStat.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
+  dataOverallStat,
 } from "./data/index.js";
 
 
@@ -57,6 +58,7 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     // ONLY ADD DATA ONE TIME
+    //OverallStat.insertMany(dataOverallStat);
     //Product.insertMany(dataProduct);
     //ProductStat.insertMany(dataProductStat);
     //Transaction.insertMany(dataTransaction);
