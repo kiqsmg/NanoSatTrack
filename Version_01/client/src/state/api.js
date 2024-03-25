1583493f23622bcb47a2610b26827d7152ab5a9c
kiqsmg <kiqsmg@gmail.com>
2024-03-25 10:08:33 -0300

    Refactor usegetquery code

--- Version_01/client/src/state/api.js
+++ Version_01/client/src/state/api.js
@@ -1,53 +1,53 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const api = createApi({ //Create the API
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }), //configures the base function the consult utilizes
-    reducerPath: "adminApi", //defines the way that the reducers will be stored
-    tagTypes: [ //defines the kind of tags disposible for use
-        "User",
-        "Products",
-        "Customers",
-        "Transactions",
-        "Sales",
-    ],
-    endpoints: (build) => ({ //where we will define each endpoint
-        getUser: build.query({
-            query: (id) => `general/user/${id}`,
-            providesTags: ["User"],
-        }),
-        getProducts: build.query({
-            query: () => "client/products",
-            providesTags: ["Products"],
-        }),
-        getCustomers: build.query({
-            query: () => "client/customers",
-            providesTags: ["Customers"],
-        }),
-        getTransactions: build.query({
-            query: ({ page, pageSize, sort, search }) => ({
-                url: "client/transactions",
-                method: "GET",
-                params: { page, pageSize, sort, search },
-            }),
-            prodidesTags: ["Transactions"],
-        }),
-        getGeography: build.query({
-            query: () => "client/geography",
-            providesTags: ["Geography"],
-        }),
-        getSales: build.query({
-            query: () => "sales/sales",
-            providesTags: ["Sales"],
-        }),
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  reducerPath: "adminApi",
+  tagTypes: [
+    "User",
+    "Products",
+    "Customers",
+    "Transactions",
+    "Geography",
+    "Sales",
+  ],
+  endpoints: (build) => ({
+    getUser: build.query({
+      query: (id) => `general/user/${id}`,
+      providesTags: ["User"],
     }),
+    getProducts: build.query({
+      query: () => "client/products",
+      providesTags: ["Products"],
+    }),
+    getCustomers: build.query({
+      query: () => "client/customers",
+      providesTags: ["Customers"],
+    }),
+    getTransactions: build.query({
+      query: ({ page, pageSize, sort, search }) => ({
+        url: "client/transactions",
+        method: "GET",
+        params: { page, pageSize, sort, search },
+      }),
+      providesTags: ["Transactions"],
+    }),
+    getGeography: build.query({
+      query: () => "client/geography",
+      providesTags: ["Geography"],
+    }),
+    getSales: build.query({
+      query: () => "sales/sales",
+      providesTags: ["Sales"],
+    }),
+  }),
 });
 
-
 export const {
-    useGetUserQuery,
-    useGetProductsQuery,
-    useGetCustomersQuery,
-    useGetTransactionsQuery,
-    useGetGeographyQuery,
-    useGetSalesQuery,
+  useGetUserQuery,
+  useGetProductsQuery,
+  useGetCustomersQuery,
+  useGetTransactionsQuery,
+  useGetGeographyQuery,
+  useGetSalesQuery,
 } = api;
