3eec47989ed05e5c420f2cb0b37070c45ea09736
kiqsmg <kiqsmg@gmail.com>
2024-03-25 11:49:05 -0300

    Import Breakdown from scenes and create route for the page

--- Version_01/client/src/App.js
+++ Version_01/client/src/App.js
@@ -14,6 +14,7 @@ import Geography from "scenes/geography";
 import Overview from "scenes/overview";
 import Daily from "scenes/daily";
 import Monthly from "scenes/monthly";
+import Breakdown from "scenes/breakdown";
 
 
 function App() {
@@ -36,7 +37,7 @@ function App() {
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
               <Route path="/monthly" element={<Monthly />} />
-
+              <Route path="/breakdown" element={<Breakdown />} />
 
             </Route>
           </Routes>
