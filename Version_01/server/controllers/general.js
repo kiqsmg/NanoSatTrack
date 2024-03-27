57722763383eef2793f7f1dc49d4b55468789212
kiqsmg <kiqsmg@gmail.com>
2024-03-27 15:54:45 -0300

    Create getDashboardStat function to handle requests to retrive statistics for the dashboard page.

--- Version_01/server/controllers/general.js
+++ Version_01/server/controllers/general.js
@@ -1,4 +1,7 @@
 import User from "../models/User.js";
+import OverallStat from "../models/OverallStat.js";
+import Transaction from "../models/Transaction.js";
+
 
 
 export const getUser = async (req, res) => {
@@ -6,6 +9,53 @@ export const getUser = async (req, res) => {
     const { id } = req.params;
     const user = await User.findById(id);
     res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+
+export const getDashboardStats = async (req, res) => { //this endpoint will retrive all info in dashboard page
+  try {
+    //hardcoded values... basically we are creating data because our test data does not have this
+    const currentMonth = "November";
+    const currentYear = 2021;
+    const currentDay = "2021-11-15";
+
+    /* Recent Transactions */
+    const transactions = await Transaction.find()
+      .limit(50)
+      .sort({ createdOn: -1 });
+
+    /* Overall Stats */
+    const overallStat = await OverallStat.find({ year: currentYear });
+
+    const {
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+    } = overallStat[0];
+
+    const thisMonthStats = overallStat[0].monthlyData.find(({ month }) => {
+      return month === currentMonth;
+    });
+
+    const todayStats = overallStat[0].dailyData.find(({ date }) => {
+      return date === currentDay;
+    });
+
+    res.status(200).json({
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      salesByCategory,
+      thisMonthStats,
+      todayStats,
+      transactions,
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
