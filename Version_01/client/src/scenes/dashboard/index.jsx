7a4b1fe8b96ae01c2899b461059486477d99cf01
kiqsmg <kiqsmg@gmail.com>
2024-03-28 22:21:25 -0300

    Add Imports for the dashboard page

--- Version_01/client/src/scenes/dashboard/index.jsx
+++ Version_01/client/src/scenes/dashboard/index.jsx
@@ -1,4 +1,25 @@
 import React from "react";
+import FlexBetween from "components/FlexBetween";
+import Header from "components/Header";
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traffic,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import BreakdownChart from "components/BreakdownChart";
+import OverviewChart from "components/OverviewChart";
+import { useGetDashboardQuery } from "state/api";
+
 
 const Dashboard = () => {
     return <div>Dashboard</div>;
