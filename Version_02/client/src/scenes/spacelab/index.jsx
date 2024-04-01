94dfc36555e6baf915aeac0a18f8e12bf90bb33f
kiqsmg <kiqsmg@gmail.com>
2024-04-01 10:11:54 -0300

    Fix Spacelab main content img

--- Version_02/client/src/scenes/spacelab/index.jsx
+++ Version_02/client/src/scenes/spacelab/index.jsx
@@ -14,6 +14,11 @@ import Paper from '@mui/material/Paper';
 import Grid from "@mui/material/Grid";
 import Link from '@mui/material/Link';
 import cubesatBackground from "../../assets/cubesat_background.webp";
+import FloripaSat01 from "../../assets/FloripaSat-1.jpg";
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
 
 const Dashboard = () => {
@@ -22,7 +27,7 @@ const Dashboard = () => {
   return (
     <Box m="1.5rem 2.5rem">
       <FlexBetween>
-        <Header title="NanosatTracker" subtitle="Welcome to NanosatTracker" />
+        <Header title="Welcome to NanosatTracker"/>
 
         <Box>
           <Button
@@ -82,7 +87,7 @@ const Dashboard = () => {
                   Spacelab- NanosatTracker
                 </Typography>
                 <Typography variant="h5" color="inherit" paragraph>
-                  Developing new systems to bring data to earth
+                  Developing new systems to bring data from space to earth
                 </Typography>
                 <Link variant="subtitle1" href="#">
                   GitHub
@@ -91,6 +96,49 @@ const Dashboard = () => {
             </Grid>
           </Grid>
         </Paper>
+
+        <Box>
+          <h1>Overview</h1>
+          <p>SpaceLab brings together several research groups from the Federal University of Santa Catarina (UFSC), Brazil.
+            The different groups conduct research and development activities in space systems in general,
+            aiming to make space more accessible not only to the scientific community but also to the industry.
+            Featured missions include FloripaSat-1, FloripaSat-2, and GOMX-5.
+            This organization combines repositories for different projects, files and documents. The following sections briefly
+            describes the main repositories and their relation to the SpaceLab's major space campaigns.
+          </p>
+
+          <img src="ufscImage.png" alt="" />
+          
+        </Box>
+
+        <Box>
+          <Grid item xs={12} md={6}>
+            <CardActionArea component="a" href="#">
+              <Card sx={{ display: 'flex' }}>
+                <CardContent sx={{ flex: 1 }}>
+                  <Typography component="h2" variant="h5">
+                    Our Institution
+                  </Typography>
+                  <Typography variant="subtitle1" color="text.secondary">
+                    Post date
+                  </Typography>
+                  <Typography variant="subtitle1" paragraph>
+                    Post description
+                  </Typography>
+                  <Typography variant="subtitle1" color="primary">
+                    Continue reading...
+                  </Typography>
+                </CardContent>
+                <CardMedia
+                  component="img"
+                  sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
+                  image= {FloripaSat01}
+                  alt="Imagem do FloripaSat-1"
+                />
+              </Card>
+            </CardActionArea>
+          </Grid>
+        </Box>
       </Box>
     </Box>
   );
