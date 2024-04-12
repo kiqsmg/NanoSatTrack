9d8d1ae7fa099833a62c598cae0bb0001c3d37d9
kiqsmg <kiqsmg@gmail.com>
2024-04-12 10:26:03 -0300

    Crate new data index document to test new data modeling dividing all the satellite data into 6 different calls: (satellite; date; connectivity; temperatures; sp_temp; battery_overall)

---
+++ Version_02/server/data/index4.js
@@ -0,0 +1,35 @@
+export const satellite =[
+    {
+        satelitte_id: floripasat1,
+        date_id: 1,
+        temperatures_id: 1,
+        sp_temp_id: 1,
+        battery_overall_id: 1,
+        connectivity_id: 1,
+    },
+    {
+        satelitte_id: floripasat1,
+        date_id: 2,
+        temperatures_id: 2,
+        sp_temp_id: 2,
+        battery_overall_id: 2,
+        connectivity_id: 2,
+    },
+    {
+        satelitte_id: floripasat1,
+        date_id: 3,
+        temperatures_id: 3,
+        sp_temp_id: 3,
+        battery_overall_id: 3,
+        connectivity_id: 3,
+    },
+    {
+        satelitte_id: floripasat1,
+        date_id: 4,
+        temperatures_id: 4,
+        sp_temp_id: 4,
+        battery_overall_id: 4,
+        connectivity_id: 4,
+    },   
+];
+
