20e659716b8b3fbe8d438298926f8ce14b0e3115
kiqsmg <kiqsmg@gmail.com>
2024-04-16 10:56:55 -0300

    Change DateSchema for a better model to connect to mongooDB

--- Version_02/server/models/Date.js
+++ Version_02/server/models/Date.js
@@ -1,15 +1,31 @@
 import mongoose from "mongoose";
 
-const DateSchema = new mongoose.Schema(
-  {
-    year: Number,
-    month: Number,
-    day: Number,
-    hour: Number,
-    minute: Number,
-    second: Number,
+const DateSchema = new mongoose.Schema({
+  year: {
+    type: Number,
+    required: true
+  },
+  month: {
+    type: Number,
+    required: true
+  },
+  day: {
+    type: Number,
+    required: true
+  },
+  hour: {
+    type: Number,
+    required: true
+  },
+  minute: {
+    type: Number,
+    required: true
+  },
+  second: {
+    type: Number,
+    required: true
   }
-);
+});
 
 const Date = mongoose.model("Date", DateSchema);
 export default Date;
