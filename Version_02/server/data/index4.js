f8eef027bd2fa01e340bef3a50bf7a6d974bfe9f
kiqsmg <kiqsmg@gmail.com>
2024-04-22 09:20:06 -0300

    Rearange the satellites data information

--- Version_02/server/data/index4.js
+++ Version_02/server/data/index4.js
@@ -1,42 +1,34 @@
 export const dataSatellite =[
     {
-        _id: "floripasat1",
-        dates: [
-            "1918273",
-            "2198237",
-            "3091823",
-            "4839021",
-        ],
-        temperatures: [
-            "1918273",
-            "2198237",
-            "3091823",
-            "4839021",
-        ],
-        sp_temperatures: [
-            "1918273",
-            "2198237",
-            "3091823",
-            "4839021",
-        ],
-        battery_overall: [
-            "1918273",
-            "2198237",
-            "3091823",
-            "4839021",
-        ],
-        connectivities: [
-            "1918273",
-            "2198237",
-            "3091823",
-            "4839021",
-        ],
+        _id: "63701d24f03239c72c00018e",
+        name: "Floripasat-1",
+        category: "1U cubesat",
+        description: "FloripaSat-1 is a platform with five modules including core components for mission control and payloads, featuring an amateur radio repeater for global emergency and rescue communications.",
+    },
+    {
+        _id: "63701d24f03239c72c00018f",
+        name: "Golds Ufsc",
+        category: "2U cubesat",
+        description: "GOLDS-UFSC is a service module for INPE’s EDC payload, and also a platform for the test of core spacecraft technologies in a microgravity, high-radiation and low Earth orbit environment.",
+    },
+    {
+        _id: "63701d24f03239c72c000190",
+        name: "Catarina-A1",
+        category: "2U cubesat",
+        description: "Catarina-A1 is the first satellite of Catarina Constelation, with the main objective of receiving data transmitted by data collection platforms installed in remote areas.",
+    },
+    {
+        _id: "63701d24f03239c72c000191",
+        name: "Catarina-A3",
+        category: "2U cubesat",
+        description: "Catarina-A1 is the first satellite of Catarina Constelation, with the main objective of receiving data transmitted by data collection platforms installed in remote areas.",
     },
 ];
 
 export const dataDate =[
     {
-        _id: "1918273",
+        _id: "f1918273",
+        satellite_id: "63701d24f03239c72c00018e",
         year: 2019,
         month: 12,
         day: 20,
@@ -45,7 +37,8 @@ export const dataDate =[
         second: 22,
     },
     {
-        _id: "2198237",
+        _id: "f2198237",
+        satellite_id: "63701d24f03239c72c00018e",
         year: 2019,
         month: 12,
         day: 20,
@@ -54,7 +47,8 @@ export const dataDate =[
         second: 32,
     },
     {
-        _id: "3091823",
+        _id: "f3091823",
+        satellite_id: "63701d24f03239c72c00018e",
         year: 2019,
         month: 12,
         day: 20,
@@ -63,7 +57,8 @@ export const dataDate =[
         second: 42,
     },
     {
-        _id: "4839021",
+        _id: "f4839021",
+        satellite_id: "63701d24f03239c72c00018e",
         year: 2019,
         month: 12,
         day: 20,
@@ -76,24 +71,28 @@ export const dataDate =[
 export const dataTemperatures =[
     {
         _id: "1",
+        satellite_id: "63701d24f03239c72c00018e",
         date_id: "1918273",
         eps_temperature: 13.6149,
         battery_temperature: 3.875,
     },
     {
         _id: "2",
+        satellite_id: "63701d24f03239c72c00018e",
         date_id: "2198237",
         eps_temperature: 13.3635,
         battery_temperature: 3.875,
     },
     {
         _id: "3",
+        satellite_id: "63701d24f03239c72c00018e",
         date_id: "3091823",
         eps_temperature: 13.1121,
         battery_temperature: 3.625,
     },
     {
         _id: "4",
+        satellite_id: "63701d24f03239c72c00018e",
         date_id: "4839021",
         eps_temperature: 12.6094,
         battery_temperature: 3.875,
