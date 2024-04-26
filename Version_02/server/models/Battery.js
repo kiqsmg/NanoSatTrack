b46b9e8e0dcffe231ec8bef2236b981ba005d4b3
kiqsmg <kiqsmg@gmail.com>
2024-04-26 12:05:40 -0300

    Create Nattery Schema

---
+++ Version_02/server/models/Battery.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const BatterySchema = new mongoose.Schema({
+  date_id: { type: mongoose.Types.ObjectId, ref: "Battery_overall" },
+
+  battery_cell_1_voltage: {
+    type: Number,
+    required: true,
+  },
+  battery_cell_2_voltage: {
+    type: Number,
+    required: true,
+  },
+  battery_current: {
+    type: Number,
+    required: true,
+  },
+  battery_temperature: {
+    type: Number,
+    required: true,
+  },
+  battery_charge: {
+    type: Number,
+    required: true,
+  },
+  energy_level: {
+    type: Number,
+    required: true,
+  },
+
+});
+
+const BatteryOverall = mongoose.model("Battery_overall", BatterySchema);
+export default BatteryOverall;
