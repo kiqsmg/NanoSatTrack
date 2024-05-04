3320fa2c1dcc18f64c9f6c50d8f68eb6ac2ed7d2
kiqsmg <kiqsmg@gmail.com>
2024-05-03 22:11:00 -0300

    Refactor api code in client side

--- Version_02/client/src/state/api.js
+++ Version_02/client/src/state/api.js
@@ -2,15 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  // reducerPath: "adminApi",
   tagTypes: [
-    "User",
-    "Products",
-    "Customers",
-    "Transactions",
-    "Geography",
-    "Sales",
-    "Admins",
-    "Performance",
     "Dashboard",
     "Satellite",
     "Battery",
@@ -24,44 +17,12 @@ export const api = createApi({
       query: () => "client/products",
       providesTags: ["Products"],
     }),
-    getCustomers: build.query({
-      query: () => "client/customers",
-      providesTags: ["Customers"],
-    }),
-    getTransactions: build.query({
-      query: ({ page, pageSize, sort, search }) => ({
-        url: "client/transactions",
-        method: "GET",
-        params: { page, pageSize, sort, search },
-      }),
-      providesTags: ["Transactions"],
-    }),
-    getGeography: build.query({
-      query: () => "client/geography",
-      providesTags: ["Geography"],
-    }),
-    getSales: build.query({
-      query: () => "sales/sales",
-      providesTags: ["Sales"],
-    }),
-    getAdmins: build.query({
-      query: () => "management/admins",
-      providesTags: ["Admins"],
-    }),
-    getUserPerformance: build.query({
-      query: (id) => `management/performance/${id}`,
-      providesTags: ["Performance"],
-    }),
-    getDashboard: build.query({
-      query: () => "general/dashboard",
-      providesTags: ["Dashboard"],
-    }),
     getSatellite: build.query({
       query: () => "general/satellite",
       providesTags: ["Satellite"],
     }),
     getBattery: build.query({
-      query: () => "battery",
+      query: () => "floripasat1/battery",
       providesTags: ["Battery"],
     }),
   }),
@@ -70,13 +31,6 @@ export const api = createApi({
 export const {
   useGetUserQuery,
   useGetProductsQuery,
-  useGetCustomersQuery,
-  useGetTransactionsQuery,
-  useGetGeographyQuery,
-  useGetSalesQuery,
-  useGetAdminsQuery,
-  useGetUserPerformanceQuery,
-  useGetDashboardQuery,
   useGetSatelliteQuery,
   useGetBatteryQuery,
 } = api;
