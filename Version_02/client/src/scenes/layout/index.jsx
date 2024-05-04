2c7b22b5031b4200b619215476edd102ff34b3d4
kiqsmg <kiqsmg@gmail.com>
2024-05-04 11:44:25 -0300

    Delete code related to user login

--- Version_02/client/src/scenes/layout/index.jsx
+++ Version_02/client/src/scenes/layout/index.jsx
@@ -4,7 +4,6 @@ import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
-import { useGetUserQuery } from "state/api";
 
 
 
@@ -12,13 +11,10 @@ import { useGetUserQuery } from "state/api";
 const Layout = () => {
     const isNonMobile = useMediaQuery("(min-width: 600px)");  //identify if the screen is mobile or not
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const userId = useSelector((state) => state.global.userId);
-    const { data } = useGetUserQuery(userId);
 
     return (
         <Box display={isNonMobile ? "flex" : "block" } /* flex for desktop and block for mobile */  width= "100%" height= "100%">
             <Sidebar 
-                user={ data || {} }
                 isNonMobile={isNonMobile}
                 drawerWidth="300px"
                 isSidebarOpen={isSidebarOpen}
@@ -26,7 +22,6 @@ const Layout = () => {
             />
             <Box flexGrow={1}>
                 <Navbar
-                    user={ data || {} }
                     isSidebarOpen={isSidebarOpen}
                     setIsSidebarOpen={setIsSidebarOpen}
                 />
