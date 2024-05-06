e774540458567b67364f25848058dbcc4009e776
kiqsmg <kiqsmg@gmail.com>
2024-05-06 13:39:52 -0300

    Fix gneral satellites controller GetSatellite query, by changing findby to just find

--- Version_02/server/controllers/general.js
+++ Version_02/server/controllers/general.js
@@ -2,8 +2,7 @@ import Satellite from "../models/Satellite.js";
 
 export const getSatellite = async (req, res) => {
   try {
-    const { satellite_id } = req.params;
-    const satellite = await Satellite.findById(satellite_id);
+    const satellite = await Satellite.find();
     res.status(200).json(satellite);
   } catch (error) {
     res.status(404).json({ message: error.message });
