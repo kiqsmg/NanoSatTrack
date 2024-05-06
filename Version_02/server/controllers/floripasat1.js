a6bc36474be51e9cf840a8d8a25b96ae9f3b2b67
kiqsmg <kiqsmg@gmail.com>
2024-05-05 23:14:52 -0300

    Fix controllers bugs

--- Version_02/server/controllers/floripasat1.js
+++ Version_02/server/controllers/floripasat1.js
@@ -1,45 +1,13 @@
 
-import Date from "../models/Date.js";
+import Battery from "../models/Battery.js";
 
-
-//Date query
-export const getDate = async (req, res) => {  //route handler(fetch date)
-    try {
-        //sort should look like this: { "field": "userId", "sort": "desc"}
-        const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;  //Parsing Request Parameters(extract parameters from the requesr query string)
-
-        //formated sort should look like this {userId: -1}
-        const generateSort = () => { //formats the sort to MongoDB sor format
-            const sortParsed = JSON.parse(sort);
-            const sortFormatted = {
-                [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
-            };
-
-            return sortFormatted;
-        };
-        const sortFormatted = Boolean(sort) ? generateSort() : {};
-
-        const date = await Date.find({ //consults the data base using the search criteria and pagination paramters
-            $or: [
-                { cost: { $regex: new RegExp(search, "i") } },
-                { userId: { $regex: new RegExp(search, "i") } },          
-            ],
-        })
-            .sort(sortFormatted)
-            .skip(page * pageSize)
-            .limit(pageSize);
-
-
-        const total = await Date.countDocuments ({ //counts the number of documents in the collections based on the search
-            name: { $regex: search, $options: "i" },
-        });
-
-
-        res.status(200).json({ //sends response
-            date,
-            total,
-        });
-    } catch (error) {  //error handling
-        res.status(404).json({ message: error.message});
-    }
+//const { date_id } = req.params;
+//Battery query
+export const getBattery = async (req, res) => {
+  try {
+    const battery = await Battery.findById();
+    res.status(200).json(battery);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
--- Version_02/server/controllers/general.js
+++ Version_02/server/controllers/general.js
@@ -2,8 +2,8 @@ import Satellite from "../models/Satellite.js";
 
 export const getSatellite = async (req, res) => {
   try {
-    //const { satellite_id } = req.params;
-    const satellite = await Satellite.findById(id);
+    const { satellite_id } = req.params;
+    const satellite = await Satellite.findById(satellite_id);
     res.status(200).json(satellite);
   } catch (error) {
     res.status(404).json({ message: error.message });
