960e9345b47a56b82b3b70e78672a2d2ad8efb4c
kiqsmg <kiqsmg@gmail.com>
2024-05-11 18:59:01 -0300

    Create Downlink controller in floripasat1

--- Version_02/server/controllers/floripasat1.js
+++ Version_02/server/controllers/floripasat1.js
@@ -1,5 +1,6 @@
 
 import BatteryOverall from "../models/Battery.js";
+import FloripaSat1Overall from "../models/FloripaSat1.js";
 
 //Battery query
 export const getBattery = async (req, res) => {
@@ -8,5 +9,17 @@ export const getBattery = async (req, res) => {
     res.status(200).json(battery);
   } catch (error) {
     res.status(404).json({ message: error.message });
+  }
+};
+
+
+//Downlinks query
+
+export const getDownlink = async (req, res) => {
+  try {
+      const downlink = await FloripaSat1Overall.find();
+      res.status(200).json(downlink)
+  } catch( error ) {
+      res.status(404).json({message: error})
   }
 };
