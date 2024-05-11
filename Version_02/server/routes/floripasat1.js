a10a6473eb0a6d9855f23a54f94729040f6c7b8a
kiqsmg <kiqsmg@gmail.com>
2024-05-11 18:54:19 -0300

    Create route for Downlink in the floripasat1 routes

--- Version_02/server/routes/floripasat1.js
+++ Version_02/server/routes/floripasat1.js
@@ -1,10 +1,13 @@
 import express from "express";
 import {
   getBattery,
+  getDownlink
 } from "../controllers/floripasat1.js";
 
 const router = express.Router();
 
 router.get("/battery", getBattery);
+router.get("/downlink", getDownlink);
+
 
 export default router;
