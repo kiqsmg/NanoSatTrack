42ff32ee7f4fb18de4d298ad97e8dd98dd831277
kiqsmg <kiqsmg@gmail.com>
2024-05-14 10:47:07 -0300

    Fix flex size on the info display for the columns od data

--- Version_02/client/src/scenes/downlinks/index.jsx
+++ Version_02/client/src/scenes/downlinks/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Box, useTheme} from "@mui/material"
-import { useGetDownlinkQuery } from "../../state/api"
 import Header from "../../components/Header";
 import {DataGrid} from "@mui/x-data-grid";
+import { useGetDownlinkQuery } from "../../state/api"
+
 
 function Downlink() {
   const theme = useTheme();
@@ -17,47 +18,47 @@ function Downlink() {
     {
       field: "name",
       headerName: "NAME",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "year",
       headerName: "YEAR",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "month",
       headerName: "MONTH",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "day",
       headerName: "DAY",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "hour",
       headerName: "HOUR",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "minute",
       headerName: "MINUTE",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "second",
       headerName: "SECOND",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "battery_cell_1_voltage",
       headerName: "BATTERY CELL 1 VOL.",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "battery_cell_2_voltage",
       headerName: "BATTERY CELL 2 VOL.",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "battery_temperature",
@@ -67,82 +68,82 @@ function Downlink() {
     {
       field: "battery_current",
       headerName: "BATTERY CURRENT",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "battery_charge",
       headerName: "BATTERY CHARGE",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "sp_01_current",
       headerName: "Solar panel 1 current",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "sp_02_current",
       headerName: "Solar panel 2 current",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "sp_03_current",
       headerName: "Solar panel 3 current",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "sp_04_current",
       headerName: "Solar panel 4 current",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "sp_05_current",
       headerName: "Solar panel 5 current",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "sp_06_current",
       headerName: "Solar panel 6 current",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "sp_01_02_voltage",
       headerName: "Solar panel 1 & 2 voltage",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "sp_03_04_voltage",
       headerName: "Solar panel 3 & 4 voltage",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "sp_05_06_voltage",
       headerName: "Solar panel 5 & 6 voltage",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "energy_level",
       headerName: "ENERGY LEVEL",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "eps_temperature",
       headerName: "EPS TEMPERATURE",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "satNOGS",
       headerName: "satNOGS",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "callsign",
       headerName: "CALLSIGN",
-      flex: 0.5
+      flex: 1
     },
     {
       field: "grid_locator",
       headerName: "GRID LOCATOR",
-      flex: 0.5
+      flex: 1
     },
   ]
   return (
