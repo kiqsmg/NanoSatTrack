7976c09d00fdb10548a21bf9d5395d6679b64ebc
kiqsmg <kiqsmg@gmail.com>
2024-05-15 09:45:59 -0300

    Make some changes in the downlink UI

--- Version_02/client/src/scenes/downlinks/index.jsx
+++ Version_02/client/src/scenes/downlinks/index.jsx
@@ -13,111 +13,111 @@ function Downlink() {
     {
       field: "_id",
       headerName: "ID",
-      flex: 1
+      flex: 4
     },
     {
       field: "name",
       headerName: "NAME",
-      flex: 1
+      flex: 1.5
     },
     {
       field: "year",
       headerName: "YEAR",
-      flex: 1
+      flex: 0.5
     },
     {
       field: "month",
-      headerName: "MONTH",
-      flex: 1
+      headerName: "MON",
+      flex: 0.5
     },
     {
       field: "day",
       headerName: "DAY",
-      flex: 1
+      flex: 0.5
     },
     {
       field: "hour",
       headerName: "HOUR",
-      flex: 1
+      flex: 0.5
     },
     {
       field: "minute",
-      headerName: "MINUTE",
-      flex: 1
+      headerName: "MIN",
+      flex: 0.5
     },
     {
       field: "second",
-      headerName: "SECOND",
-      flex: 1
+      headerName: "SEC",
+      flex: 0.5
     },
     {
       field: "battery_cell_1_voltage",
-      headerName: "BATTERY CELL 1 VOL.",
-      flex: 1
+      headerName: "BAT1[V]",
+      flex: 1.2
     },
     {
       field: "battery_cell_2_voltage",
-      headerName: "BATTERY CELL 2 VOL.",
-      flex: 1
+      headerName: "BAT2[V]",
+      flex: 1.2
     },
     {
       field: "battery_temperature",
-      headerName: "BATTERY TEMP.",
-      flex: 1
+      headerName: "BAT[°C].",
+      flex: 1.2
     },
     {
       field: "battery_current",
-      headerName: "BATTERY CURRENT",
-      flex: 1
+      headerName: "BAT[A]",
+      flex: 1.2
     },
     {
       field: "battery_charge",
-      headerName: "BATTERY CHARGE",
+      headerName: "CHARGE",
       flex: 1
     },
     {
       field: "sp_01_current",
-      headerName: "Solar panel 1 current",
+      headerName: "SP 1[A]",
       flex: 1
     },
     {
       field: "sp_02_current",
-      headerName: "Solar panel 2 current",
+      headerName: "SP 2[A]",
       flex: 1
     },
     {
       field: "sp_03_current",
-      headerName: "Solar panel 3 current",
+      headerName: "SP 3[A]",
       flex: 1
     },
     {
       field: "sp_04_current",
-      headerName: "Solar panel 4 current",
+      headerName: "SP 4[A]",
       flex: 1
     },
     {
       field: "sp_05_current",
-      headerName: "Solar panel 5 current",
+      headerName: "SP 5[A]",
       flex: 1
     },
     {
       field: "sp_06_current",
-      headerName: "Solar panel 6 current",
+      headerName: "SP 6[A]",
       flex: 1
     },
     {
       field: "sp_01_02_voltage",
-      headerName: "Solar panel 1 & 2 voltage",
+      headerName: "SP 1-2[V]",
       flex: 1
     },
     {
       field: "sp_03_04_voltage",
-      headerName: "Solar panel 3 & 4 voltage",
+      headerName: "SP 3-4[V]",
       flex: 1
     },
     {
       field: "sp_05_06_voltage",
-      headerName: "Solar panel 5 & 6 voltage",
+      headerName: "SP 5-6[V]",
       flex: 1
     },
     {
@@ -127,7 +127,7 @@ function Downlink() {
     },
     {
       field: "eps_temperature",
-      headerName: "EPS TEMPERATURE",
+      headerName: "EPS[°C]",
       flex: 1
     },
     {
@@ -142,15 +142,15 @@ function Downlink() {
     },
     {
       field: "grid_locator",
-      headerName: "GRID LOCATOR",
+      headerName: "LOCATOR",
       flex: 1
     },
   ]
   return (
     <>
-    <Box m="1.5rem 2.5rem">
+    <Box m="1.5rem 0.5rem">
         <Header title="DOWNLINKS" subtitle="List Of all Downlinks" />
-    <Box mt="40px" height="75vh"
+    <Box mt="40px" height="75vh" width="100%"
         sx={{ "& .MuiDataGrid-root" : { 
             border: "none",
         },
