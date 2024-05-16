6ce3aa16029b2d7b5195a79609d1d5ce3a6e3564
kiqsmg <kiqsmg@gmail.com>
2024-05-16 14:44:12 -0300

    Create solarpanels page

--- Version_02/client/src/scenes/solarpanel/index.jsx
+++ Version_02/client/src/scenes/solarpanel/index.jsx
@@ -2,27 +2,47 @@ import React, { useMemo, useState } from "react";
 import { Box, useTheme } from "@mui/material";
 import Header from "../../components/Header";
 import { ResponsiveLine } from "@nivo/line";
-import { useGetSalesQuery } from "../../state/api";
+import { useGetDownlinkQuery } from "../../state/api";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-const Daily = () => {
-  const [startDate, setStartDate] = useState(new Date("2021-02-01"));
-  const [endDate, setEndDate] = useState(new Date("2021-03-01"));
-  const { data } = useGetSalesQuery();
+const Solarpanel = () => {
+  const [startDate, setStartDate] = useState(new Date("2019-12-01"));
+  const [endDate, setEndDate] = useState(new Date("2020-03-01"));
+  const { data } = useGetDownlinkQuery();
   const theme = useTheme();
 
   const [formattedData] = useMemo(() => {
     if (!data) return [];
 
     const { dailyData } = data;
-    const totalSalesLine = {
-      id: "totalSales",
+    const sp_01_currentLine = {
+      id: "sp_01_current",
       color: theme.palette.secondary.main,
       data: [],
     };
-    const totalUnitsLine = {
-      id: "totalUnits",
+    const sp_02_currentLine = {
+      id: "sp_02_current",
+      color: theme.palette.secondary[600],
+      data: [],
+    };
+    const sp_03_currentLine = {
+      id: "sp_03_current",
+      color: theme.palette.secondary[600],
+      data: [],
+    };
+    const sp_04_currentLine = {
+      id: "sp_04_current",
+      color: theme.palette.secondary[600],
+      data: [],
+    };
+    const sp_05_currentLine = {
+      id: "sp_05_current",
+      color: theme.palette.secondary[600],
+      data: [],
+    };
+    const sp_06_currentLine = {
+      id: "sp_06_current",
       color: theme.palette.secondary[600],
       data: [],
     };
@@ -32,18 +52,35 @@ const Daily = () => {
       if (dateFormatted >= startDate && dateFormatted <= endDate) {
         const splitDate = date.substring(date.indexOf("-") + 1);
 
-        totalSalesLine.data = [
-          ...totalSalesLine.data,
+        sp_01_currentLine.data = [
+          ...sp_01_currentLine.data,
           { x: splitDate, y: totalSales },
         ];
-        totalUnitsLine.data = [
-          ...totalUnitsLine.data,
-          { x: splitDate, y: totalUnits },
+        sp_02_currentLine.data = [
+          ...sp_02_currentLine.data,
+          { x: splitDate, y: totalSales },
         ];
+        sp_03_currentLine.data = [
+          ...sp_03_currentLine.data,
+          { x: splitDate, y: totalSales },
+        ];
+        sp_04_currentLine.data = [
+          ...sp_04_currentLine.data,
+          { x: splitDate, y: totalSales },
+        ];
+        sp_05_currentLine.data = [
+          ...sp_05_currentLine.data,
+          { x: splitDate, y: totalSales },
+        ];
+        sp_06_currentLine.data = [
+          ...sp_06_currentLine.data,
+          { x: splitDate, y: totalSales },
+        ];
+        
       }
     });
 
-    const formattedData = [totalSalesLine, totalUnitsLine];
+    const formattedData = [sp_01_currentLine, sp_02_currentLine, sp_03_currentLine, sp_04_currentLine, sp_05_currentLine, sp_06_currentLine];
     return [formattedData];
   }, [data, startDate, endDate]); // eslint-disable-line react-hooks/exhaustive-deps
 
@@ -184,4 +221,4 @@ const Daily = () => {
   );
 };
 
-export default Daily;
+export default Solarpanel;
