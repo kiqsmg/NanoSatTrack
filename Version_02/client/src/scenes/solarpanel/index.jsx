3113c98a8868b9738014d652326fc31a376091d2
kiqsmg <kiqsmg@gmail.com>
2024-05-19 21:08:17 -0300

    Create second solarpanel graph

--- Version_02/client/src/scenes/solarpanel/index.jsx
+++ Version_02/client/src/scenes/solarpanel/index.jsx
@@ -219,7 +219,144 @@ const Solarpanel = () => {
               },
             ]}
           />
+
+          
         ) : (
+          
+          <>Loading...</>
+        )}
+      </Box>
+
+      <Box height="75vh">
+        <Box display="flex" justifyContent="flex-end">
+          <Box>
+            <DatePicker
+              selected={startDate}
+              onChange={(dateFormatted) => setStartDate(dateFormatted)}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+            />
+          </Box>
+          <Box>
+            <DatePicker
+              selected={endDate}
+              onChange={(dateFormatted) => setEndDate(dateFormatted)}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+            />
+          </Box>
+        </Box>
+
+        {data ? (
+          <ResponsiveLine
+            data={formattedData}
+            theme={{
+              axis: {
+                domain: {
+                  line: {
+                    stroke: theme.palette.secondary[200],
+                  },
+                },
+                legend: {
+                  text: {
+                    fill: theme.palette.secondary[200],
+                  },
+                },
+                ticks: {
+                  line: {
+                    stroke: theme.palette.secondary[200],
+                    strokeWidth: 1,
+                  },
+                  text: {
+                    fill: theme.palette.secondary[200],
+                  },
+                },
+              },
+              legends: {
+                text: {
+                  fill: theme.palette.secondary[200],
+                },
+              },
+              tooltip: {
+                container: {
+                  color: theme.palette.primary.main,
+                },
+              },
+            }}
+            colors={{ datum: "color" }}
+            margin={{ top: 50, right: 50, bottom: 70, left: 60 }}
+            xScale={{ type: "point" }}
+            yScale={{
+              type: "linear",
+              min: "auto",
+              max: "auto",
+              stacked: false,
+              reverse: false,
+            }}
+            yFormat=" >-.2f"
+            curve="catmullRom"
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+              orient: "bottom",
+              tickSize: 10,
+              tickPadding: 5,
+              tickRotation: 90,
+              legend: "Month-Day",
+              legendOffset: 60,
+              legendPosition: "middle",
+            }}
+            axisLeft={{
+              orient: "left",
+              tickSize: 10,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "Current [A]",
+              legendOffset: -50,
+              legendPosition: "middle",
+            }}
+            enableGridX={false}
+            enableGridY={false}
+            pointSize={10}
+            pointColor={{ theme: "background" }}
+            pointBorderWidth={2}
+            pointBorderColor={{ from: "serieColor" }}
+            pointLabelYOffset={-12}
+            useMesh={true}
+            legends={[
+              {
+                anchor: "top-right",
+                direction: "column",
+                justify: false,
+                translateX: 50,
+                translateY: 0,
+                itemsSpacing: 0,
+                itemDirection: "left-to-right",
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: "circle",
+                symbolBorderColor: "rgba(0, 0, 0, .5)",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemBackground: "rgba(0, 0, 0, .03)",
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+
+          
+        ) : (
+          
           <>Loading...</>
         )}
       </Box>
