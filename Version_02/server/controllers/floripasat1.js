15b2abb99372b53a4d3eef8ef499cc268ac1a230
kiqsmg <kiqsmg@gmail.com>
2024-05-24 15:59:51 -0300

    Add v.0 controller for the locations page

--- Version_02/server/controllers/floripasat1.js
+++ Version_02/server/controllers/floripasat1.js
@@ -23,3 +23,27 @@ export const getDownlink = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+/*
+export const getLocation = async(req, res) => {
+  try {
+      const users = await User.find()
+
+      const mappedLocations = users.reduce((acc,{ country }) => {
+          const countryISO3 = getCountryIso3(country)
+
+          if(!acc[countryISO3]) acc[countryISO3] = 1;
+          else acc[countryISO3]++
+          return acc
+      },{});
+
+      const formattedLocations = Object.entries(mappedLocations).map(([country, count]) => {
+          return { id: country, value: count}
+      })
+
+      res.status(200).json(formattedLocations)
+  } catch ( error ) {
+      res.status(404).json({message: error})
+  }
+};
+*/
