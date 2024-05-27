003be2d90fac8aec98e5c2d99ea777dda5145287
kiqsmg <kiqsmg@gmail.com>
2024-05-27 12:18:45 -0300

    Add href references to github and spacelab site links, also make the continue reading a tooltip that pops up a larger text explaining the mission overall in more details

--- Version_02/client/src/scenes/spacelab/index.jsx
+++ Version_02/client/src/scenes/spacelab/index.jsx
@@ -9,6 +9,7 @@ import {
   Button,
   Typography,
   useTheme,
+  Tooltip,
 } from "@mui/material";
 import { useMediaQuery } from "@mui/material";
 import Paper from '@mui/material/Paper';
@@ -37,18 +38,20 @@ const Dashboard = () => {
         <Header title="Welcome to NanosatTracker"/>
 
         <Box>
-          <Button
-            sx={{
-              backgroundColor: theme.palette.secondary.light,
-              color: theme.palette.background.alt,
-              fontSize: "14px",
-              fontWeight: "bold",
-              padding: "10px 20px",
-            }}
-          >
-            <LanOutlined sx={{ mr: "10px" }} />
-            Want to know more?
-          </Button>
+          <a href="https://spacelab.ufsc.br/en/home/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+            <Button
+              sx={{
+                backgroundColor: theme.palette.secondary.light,
+                color: theme.palette.background.alt,
+                fontSize: "14px",
+                fontWeight: "bold",
+                padding: "10px 20px",
+              }}
+            >
+              <LanOutlined sx={{ mr: "10px" }} />
+              Want to know more?
+            </Button>
+          </a>
         </Box>
       </FlexBetween>
 
@@ -96,7 +99,7 @@ const Dashboard = () => {
                 <Typography variant="h5" color="inherit" paragraph>
                   Developing new systems to bring data from space to earth
                 </Typography>
-                <Link variant="subtitle1" href="#">
+                <Link variant="subtitle1" href="https://github.com/spacelab-ufsc">
                   GitHub
                 </Link>
               </Box>
@@ -112,13 +115,11 @@ const Dashboard = () => {
             Featured missions include FloripaSat-1, GOLDS-UFSC, GOMX-5, and cubesats from the Catarina Constelation.
             This organization combines repositories for different projects, files and documents.
           </p>
-
-          <img src="ufscImage.png" alt="" />
           
         </Box>
 
         <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
-          <Grid item xs={12} md={6} mb={10} mt={5} {...gridProps}>
+          <Grid item xs={12} md={6} mb={10} mt={10} {...gridProps}>
             <CardActionArea component="a" href="#">
               <Card sx={{ display: 'flex' }}>
                 <CardContent sx={{ flex: 1 }}>
@@ -132,9 +133,13 @@ const Dashboard = () => {
                   FloripaSat-1 is a platform with five modules including core components for mission control and payloads,
                     featuring an amateur radio repeater for global emergency and rescue communications.
                   </Typography>
-                  <Typography variant="subtitle1" color="primary">
-                    Continue reading...
-                  </Typography>
+                  <Tooltip title={<Typography sx={{ fontSize: '1rem' }}>
+                    The FloripaSat-1 is a technology demonstration mission entirely developed by students from the SpaceLab at the Federal University of Santa Catarina (UFSC), Brazil. It is a cube-shaped satellite composed of 5 modules. There are main modules for mission control and the payloads. The main modules developed at UFSC are the On-Board Data Handling (OBDH), Telemetry, Tracking, and Control (TT&C), Electric Power System (EPS), and Passive Attitude Control System (ACS). The payload is an amateur radio repeater, which can be used worldwide in emergency and rescue situations, for example.
+                    </Typography>}>
+                    <Typography variant="subtitle1" color="primary">
+                      Continue reading...
+                    </Typography>
+                  </Tooltip>
                 </CardContent>
                 <CardMedia
                   component="img"
@@ -160,9 +165,12 @@ const Dashboard = () => {
                     GOLDS-UFSC is a service module for INPE’s EDC payload, and also a platform for the test of core spacecraft
                      technologies in a microgravity, high-radiation and low Earth orbit environment.
                   </Typography>
-                  <Typography variant="subtitle1" color="primary">
-                    Continue reading...
-                  </Typography>
+                  <Tooltip title={<Typography sx={{ fontSize: '1rem' }}>
+                  GOLDS-UFSC is a space technology demonstration mission created by the Federal University of Santa Catarina. The main goal is to provide the service module for the Environmental Data Collector (EDC) payload from INPE-RN. The service module was developed at UFSC and it has three main components: the Electric Power System (EPS), the On-Board Data Handling (OBDH) and Telemetry, Tracking and Command (TT&C). Besides performing the EDC main functionalities, the mission will contribute to validating key technologies that will enable faster and cheaper development of future satellites reusing the same core structure. As an educational mission, it also serves to train engineering students in space mission conception, design, implementation and operation in all areas involved.                    </Typography>}>
+                    <Typography variant="subtitle1" color="primary">
+                      Continue reading...
+                    </Typography>
+                  </Tooltip>
                 </CardContent>
                 <CardMedia
                   component="img"
@@ -183,14 +191,18 @@ const Dashboard = () => {
                     Catarina-A1
                   </Typography>
                   <Typography variant="subtitle1" color="text.secondary">
-                    Post date
+                    2U-cubesat
                   </Typography>
                   <Typography variant="subtitle1" paragraph>
-                    Post description
+                    The Catarina Constellation encompasses a set of satellites with the goal to provide services, mainly, to the civil defence, contributing to the country’s sustainable socioeconomic development agenda.
                   </Typography>
-                  <Typography variant="subtitle1" color="primary">
-                    Continue reading...
-                  </Typography>
+                  <Tooltip title={<Typography sx={{ fontSize: '1rem' }}>
+                  The Catarina-A1 is a 2U CubeSat , and it is a follow up of FloripaSat-1 mission . Both FloripaSat-1 and GOLDS-UFSC are developed by SpaceLab/UFSC . GOLDS-UFSC main payload is the EDC board Environmental Data Collection, developed by INPE. The mission is part of the ``GOLDS'' constellation (``Global Open Collecting Data System''), a collaborative CubeSat constellation for environmental data collection planned as part of the Brazilian Space Program. The project started just after the launch of FloripaSat-1 (first half of 2020) and is planned to be launched in 2023. Most of the embedded electronics is partially or totally based on the FloripaSat-1 satellite, with the same and/or improved versions of the modules. In other words, this project has, at some level, a flight heritage.
+                    </Typography>}>
+                    <Typography variant="subtitle1" color="primary">
+                      Continue reading...
+                    </Typography>
+                  </Tooltip>
                 </CardContent>
                 <CardMedia
                   component="img"
