5fa52abafcd6e2ba28b782658cb5e5013f2246c2
kiqsmg <kiqsmg@gmail.com>
2024-05-28 11:22:28 -0300

    Delete Solarpane and Battery routers, since they all go to the Downlink anyway, they dont need another router

--- Version_02/server/routes/floripasat1.js
+++ Version_02/server/routes/floripasat1.js
@@ -1,16 +1,12 @@
 import express from "express";
-// import getLocation, bellow
 import {
-  getBattery,
   getDownlink,
   getLocation,
 } from "../controllers/floripasat1.js";
 
 const router = express.Router();
 
-router.get("/battery", getBattery);
 router.get("/downlink", getDownlink);
-router.get("/solarpanel", getDownlink);
 router.get("/location", getLocation);
 
 
