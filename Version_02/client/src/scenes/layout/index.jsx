056ebcd508eca931908597d68ac3028338044ef7
kiqsmg <kiqsmg@gmail.com>
2024-10-07 14:27:31 -0300

    Refactor layout index file code

--- Version_02/client/src/scenes/layout/index.jsx
+++ Version_02/client/src/scenes/layout/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Box, useMediaQuery } from "@mui/material";
 import { Outlet } from "react-router-dom";
-import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
 import Sidebar from "components/Sidebar";
 
