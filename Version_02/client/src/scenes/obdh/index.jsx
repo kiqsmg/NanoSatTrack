dba3abfbb8864bb3c39612acbe4764e57f031ae0
kiqsmg <kiqsmg@gmail.com>
2024-10-22 10:16:09 -0300

    Refactor the OBDH index code file

--- Version_02/client/src/scenes/obdh/index.jsx
+++ Version_02/client/src/scenes/obdh/index.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import {
-  Box,
-} from "@mui/material";
+import { Box } from "@mui/material";
 
 
 const OBDH = () => {
   return (
     <Box m="1.5rem 2.5rem">
-
       <Box
         mt="20px"
         gap="20px"
@@ -16,12 +13,10 @@ const OBDH = () => {
           <h1>OBDH</h1>
           <p>The On Board Data Handling data set is not yet ready to be displayed in the website. We will soon update this page!
           </p>
-          
         </Box>
       </Box>
     </Box>
   );
 };
-
 
 export default OBDH;
