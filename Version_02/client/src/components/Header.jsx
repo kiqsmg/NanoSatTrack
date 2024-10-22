e6e9bc92f8d60fcff1fc89c50ba1b7d0a15717f2
kiqsmg <kiqsmg@gmail.com>
2024-10-22 09:19:13 -0300

    Refactor the Header component

--- Version_02/client/src/components/Header.jsx
+++ Version_02/client/src/components/Header.jsx
@@ -8,13 +8,11 @@ const Header = ({ title, subtitle }) => {
             <Typography varient="h2" color={theme.palette.secondary[100]} fontWeight="bold" sx={{ mb: "5px"}} >
                 {title}
             </Typography>
-
             <Typography varient="h5" color={theme.palette.secondary[300]} >
                 {subtitle}
             </Typography>
         </Box>
     );
 };
-
 
 export default Header;
