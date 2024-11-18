98341d8ad56b680240580eae60754ae1d3bcf219
kiqsmg <kiqsmg@gmail.com>
2024-11-18 19:25:00 -0300

    Delete test version

--- Version_01/server/routes/management.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAdmins, getUserPerformance } from "../controllers/management.js";
-
-const router = express.Router();
-
-router.get("/admins", getAdmins)
-router.get("/performance/:id", getUserPerformance);
-
-export default router;
